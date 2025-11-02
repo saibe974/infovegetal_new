4eaa6ddad370f3d54788f26245de7c5047bef83d
saibe <contact@devali.fr>
2025-11-02 16:34:54 +0400

    _frontend : 1er jet

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -59,7 +59,9 @@ export default withAppLayout(breadcrumbs, ({ collection, q }: Props) => {
     return (
         <div>
             {/* @ts-ignore react-sticky-el types don't include className in our setup */}
-            <BasicSticky stickyClassName="bg-background relative z-20" className="relative z-20">
+            <h3>FrontEnd</h3>
+            <BasicSticky stickyClassName="bg-background relative z-20"
+            >
                 <div className="flex items-center py-2 relative w-full">
                     <div className="w-200 left-0 top-1 mr-2" >
                         <SearchSoham
